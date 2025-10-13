00a38357232a681f6b32744ced73af5795f69c07
WeiZhe <41343134@nfu.edu.tw>
2025-10-13 20:11:54 +0800

    Update homework1_1_2Ackermann Function.cpp

--- homework1/src/homework1_1_2Ackermann Function.cpp
+++ homework1/src/homework1_1_2Ackermann Function.cpp
@@ -8,24 +8,24 @@ int ackermann(int m, int n) {
     const int MAX_STACK = 100000; // 可依需求調整
     int stack[MAX_STACK];
     int top = 0;
-    stack[top++] = m;
+    stack[top++] = m;//把第一層 m 推入堆疊
 
     while (top > 0) {
-        m = stack[--top];
+        m = stack[--top];// 取出最上層 m
         if (m == 0) {
             if (top == 0) {
                 return n + 1;
             }
-            n = n + 1;
+            n = n + 1;//將結果往上回傳
         }
         else if (n == 0) {
             stack[top++] = m - 1;
-            n = 1;
+            n = 1;// 對應到 A(m-1,1)
         }
         else {
             stack[top++] = m - 1;
             stack[top++] = m;
-            n = n - 1;
+            n = n - 1; // 對應到 A(m-1, A(m,n-1))
         }
     }
     return n;
