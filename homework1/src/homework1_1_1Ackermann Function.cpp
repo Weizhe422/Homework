a46af7335c67d3cca78ad0e8071e804cef99751f
WeiZhe <41343134@nfu.edu.tw>
2025-10-13 20:01:31 +0800

    Update homework1_1_1Ackermann Function.cpp

--- homework1/src/homework1_1_1Ackermann Function.cpp
+++ homework1/src/homework1_1_1Ackermann Function.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 using namespace std;
 int ack(int m, int n) {
-    if (m == 0) return n + 1; //if m=0,n+1
-    else if (n == 0) return ack(m - 1, 1); //if n=0,A(m-1,n)
-    else return ack(m - 1, ack(m, n - 1)); //otherwise , A(m-1,A(m,n-1))
-}
+   // 當 m = 0 時，直接回傳 n+1
+    if (m == 0) return n + 1; 
+   // 當 m > 0 且 n = 0 時，變成 A(m-1, 1)
+    else if (n == 0) return ack(m - 1, 1); 
+    // 其他情況 → A(m-1, A(m, n-1))
+    else return ack(m - 1, ack(m, n - 1)); 
 int main() {
     int m, n;
     cout << "輸入 m 和 n : ";
+    // 持續輸入直到手動停止
     while (cin >> m >> n) {
         cout << "(" << m << ", " << n << ") = " << ack(m, n) << endl;
         cout << "輸入 m 和 n : ";
